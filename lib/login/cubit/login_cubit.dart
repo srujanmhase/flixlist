7ba6d6e677dbb68066555c98576c1ea761c8278c
Srujan <srujanmhase5@gmail.com>
2022-10-15 01:41:07 +0530

    calling firestore repository method

--- lib/login/cubit/login_cubit.dart
+++ lib/login/cubit/login_cubit.dart
@@ -1,21 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:flixlist/auth/auth_repository.dart';
+import 'package:flixlist/services/firestore_repository.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'login_state.dart';
 part 'login_cubit.freezed.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit(this.authRepository) : super(LoginState());
+  LoginCubit(this.authRepository, this.firestoreRepository)
+      : super(LoginState());
   AuthRepository authRepository;
+  FirestoreRepository firestoreRepository;
   Future<void> signUp({
     required String email,
     required String password,
   }) async {
     emit(state.copyWith(isProcessing: true));
     try {
-      await authRepository.createUserAccount(email: email, password: password);
-      return emit(state.copyWith(isProcessing: false));
+      return await authRepository
+          .createUserAccount(email: email, password: password)
+          .then(
+            (value) => firestoreRepository.createUserRecord(
+              authRepository.getUser()?.uid ?? '',
+            ),
+          );
     } catch (e) {
       return emit(state.copyWith(isProcessing: false, error: e.toString()));
     }
