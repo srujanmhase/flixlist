38df5454c5a1748e4064f523b1c84c73a0945e4e
Srujan <srujanmhase5@gmail.com>
2022-10-14 20:57:54 +0530

    movie list tile init

---
+++ lib/movie/movie_list_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieListTile extends StatefulWidget {
+  const MovieListTile({Key? key}) : super(key: key);
+
+  @override
+  State<MovieListTile> createState() => _MovieListTileState();
+}
+
+class _MovieListTileState extends State<MovieListTile> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
